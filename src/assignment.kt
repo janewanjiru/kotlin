6d5c28e930e32849d6ce98631d7028c2f5e4f0af
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-02-20 21:37:57 +0300

    my third trial

---
+++ src/assignment.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var student= 24
+    var apples= 2
+    var total= (student * apples)
+    print(total)
+    val name="jane"
+    println(name)
+    var num1=10
+    var num2=12
+    println(num1*num2)
+    println(num1/num2)
+    println(num1-num2)
+
+
+}
