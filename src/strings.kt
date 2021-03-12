5db19a1d3977d0e5a8c07b39622eec6f1789343e
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-03-12 07:17:53 +0300

    kotlin assignment

---
+++ src/strings.kt
@@ -0,0 +1,29 @@
+fun main() {
+text()
+    var smart=join("jane",24)
+    println(smart)
+    println(name("girl"))
+    println(first("jane"))
+}
+fun text() {
+    var school="akirachix"
+    var f=school [0]
+    var g=school [2]
+    var h=school [3]
+    println("$f$g$h")
+}
+fun join(name:String,age:Int):String{
+    var name="Hi my name is $name I am $age years old"
+    return(name)
+
+
+}
+fun name(word:String){
+    println(word.length)
+}
+fun first(name: String){
+    if(name=="jane"){
+        println("That's me")
+    }else
+        println("I don't know that is")
+}
